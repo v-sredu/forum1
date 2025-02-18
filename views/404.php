84924a0c02237da5d166990506e80b94752547be
SeredaLuybov <lubasereda2003@gmail.com>
2025-02-18 15:58:58 +0300

    изменен вывод

--- views/404.php
+++ views/404.php
@@ -1,19 +1,16 @@
 <?php
 header("HTTP/1.0 404 Not Found");
+ob_start();
 ?>
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="light">
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="/public/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="/public/css/style.css">
-	<title>404 Not Found</title>
-</head>
-<body>
 <main class="row error min-vh-100 justify-content-center">
     <div class="col-4 col-lg-3 col-xl-2" style="background: url('/public/img/img-404.png') no-repeat center; background-size: contain;"></div>
     <div class="col-4 col-lg-3 col-xl-2" style="background: url('/public/img/text-404.png') no-repeat center; background-size: contain"></div>
 </main>
-<script src="/public/js/script.js"></script>
-</body>
-</html>
+<?php
+$content = ob_get_clean();
+$title = 'Ошибка 404';
+
+return template([
+	'content' => $content,
+	'title' => $title,
+], 'empty.php');
